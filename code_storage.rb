3baa2b62b41ad153368408d1df0456f4acfc1474
Eli <eghinshaw@gmail.com>
2023-05-15 15:08:57 -0500

    late commit, made circle that follows cursor, and detects mouse down, added red circle, and normal white circle to do so

---
+++ code_storage.rb
@@ -0,0 +1,23 @@
+# def movement
+#   move_left if button_down?(Gosu::KB_LEFT) || button_down?(Gosu::KB_A)
+#   move_right if button_down?(Gosu::KB_RIGHT) || button_down?(Gosu::KB_D)
+#   move_up if button_down?(Gosu::KB_UP) || button_down?(Gosu::KB_W)
+#   move_down if button_down?(Gosu::KB_DOWN) || button_down?(Gosu::KB_S)
+
+#   def move_left
+#     @circle_x -= 10
+#   end
+
+#   def move_right
+#     @circle_x += 10
+#   end
+
+#   def move_down
+#     @circle_y += 10
+#   end
+
+#   def move_up
+#     @circle_y -= 10
+#   end
+
+# end
---
+++ move-circle.rb
@@ -0,0 +1,39 @@
+require "gosu"
+
+class Test < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "Test Application"
+    @circle = Gosu::Image.new("/Users/nom/Actualize/gosu/circle.png")
+    @red_circle = Gosu::Image.new("/Users/nom/Actualize/gosu/red_circle.png")
+    @circle_radius = (@circle.width / 2)
+    @red_circle_radius = (@red_circle.width / 2)
+    @circle_scale = 0.05
+  end
+
+  def draw
+    if button_down?(Gosu::MS_LEFT)
+      @red_circle.draw(
+        mouse_x - @red_circle_radius * @circle_scale,
+        mouse_y - @red_circle_radius * @circle_scale,
+        0,
+        @circle_scale,
+        @circle_scale
+      )
+    else
+      @circle.draw(
+        mouse_x - @circle_radius * @circle_scale,
+        mouse_y - @circle_radius * @circle_scale,
+        0,
+        @circle_scale,
+        @circle_scale
+      )
+    end
+
+    if button_down?(Gosu::KB_ESCAPE)
+      close
+    end
+  end
+end
+
+Test.new.show
