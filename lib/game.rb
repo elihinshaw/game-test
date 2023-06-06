98c7499748f1426f1456d61d16cca9b20e8c1b62
Eli <eghinshaw@gmail.com>
2023-06-06 13:42:46 -0500

    added code to switch, game, cusor, and also changed background to switch 1 and switch 2

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,26 @@
+#Require gems and necessary files
 require "gosu"
 require_relative "../views/options.rb"
-require_relative "cursor.rb"
+require_relative "../views/cursor.rb"
 
 class Game < Gosu::Window
   def initialize
-    super 1920, 1080, true
+    super 1920, 1080, false
     self.caption = "Test Application"
     @background = Gosu::Color.argb(0xff2c3e50)
-    @switch1 = Switch.new
+
+    @switch1 = Switch1.new
+    @switch2 = Switch2.new
+    @switch = Gosu::Image.new("resources/untoggled-switch.jpeg")
+
     @circle = Gosu::Image.new("resources/circle.png")
     @red_circle = Gosu::Image.new("resources/red_circle.png")
-    @switch = Gosu::Image.new("resources/untoggled-switch.jpeg")
+    @cursor = Cursor.new(self)
+
     @circle_radius = (@circle.width / 2)
     @red_circle_radius = (@red_circle.width / 2)
     @circle_scale = 0.03
-    @cursor = Cursor.new(self)
+    @switch_scale = 0.1
   end
 
   def needs_cursor?
@@ -25,11 +31,18 @@ class Game < Gosu::Window
     draw_quad(0, 0, @background, width, 0, @background, width, height, @background, 0, height, @background)
 
     @switch1.draw
+    @switch2.draw
 
     @cursor.draw(mouse_x, mouse_y, button_down?(Gosu::MS_LEFT))
-
     if button_down?(Gosu::KB_ESCAPE)
       close
+    end
+  end
+
+  def update
+    p mouse_y
+    if mouse_x < 260 && mouse_y < 180
+      p "hello"
     end
   end
 end
--- views/switch.rb
+++ views/switch.rb
@@ -1,9 +1,25 @@
-class Switch < Gosu::Font
+class Switch1 < Gosu::Image
   def initialize
-    @switch1 = Gosu::Font.new(32)
+    @switch1 = Gosu::Image.new("resources/switch1.jpg")
+    @scale = 1
+    @switch_x = 0
+    @switch_y = 0
   end
 
   def draw
-    @switch1.draw_text("Options", 10, 10, 1, 1, 1, Gosu::Color::BLACK)
+    @switch1.draw(@switch_x, @switch_y, 0, @scale, @scale)
+  end
+end
+
+class Switch2 < Gosu::Image
+  def initialize
+    @switch1 = Gosu::Image.new("resources/switch2.jpg")
+    @scale = 0.5
+    @switch_x = 0
+    @switch_y = 0
+  end
+
+  def draw
+    @switch1.draw(@switch_x, @switch_y, 1, @scale, @scale)
   end
 end
